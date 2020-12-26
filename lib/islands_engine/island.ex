801304071c09abe3adce8f1d2fa6ad0d7d52c6ae
Yusent Chig <yusent@protonmail.com>
2020-12-26 15:08:40 -0800

    Add Island 'forested?' function

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -24,6 +24,10 @@ defmodule IslandsEngine.Island do
     end
   end
 
+  def forested?(%Island{coordinates: coords, hit_coordinates: hit_coords}) do
+    MapSet.equal?(coords, hit_coords)
+  end
+
   defp add_coordinates(offsets, upper_left) do
     offsets
     |> Enum.reduce_while(MapSet.new(), &add_coordinate(&2, upper_left, &1))
