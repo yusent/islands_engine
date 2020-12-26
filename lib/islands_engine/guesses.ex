0df4c0e0cff0ef934b65063aebb73d9900423709
Yusent Chig <yusent@protonmail.com>
2020-12-26 14:46:17 -0800

    Add Guesses 'add' function

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,9 +1,17 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys ~w[hits misses]a
 
   defstruct ~w[hits misses]a
 
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate) do
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate) do
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
+  end
 end
