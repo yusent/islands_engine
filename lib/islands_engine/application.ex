f162256a0f7f36cfcb86e5f8d518e4028ff73348
Yusent Chig <yusent@protonmail.com>
2021-02-25 18:45:23 -0800

    Add Game and GameSupervisor modules

--- lib/islands_engine/application.ex
+++ lib/islands_engine/application.ex
@@ -7,12 +7,13 @@ defmodule IslandsEngine.Application do
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: IslandsEngine.Worker.start_link(arg)
-      # {IslandsEngine.Worker, arg}
+      {Registry, keys: :unique, name: Registry.Game},
+      IslandsEngine.GameSupervisor
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
+    :ets.new(:game_state, [:public, :named_table])
     opts = [strategy: :one_for_one, name: IslandsEngine.Supervisor]
     Supervisor.start_link(children, opts)
   end
